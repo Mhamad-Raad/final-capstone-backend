6182a733aab9ebcf8ccb00e20712628f2d8b471e
joker69216 <hamaraad883@gmail.com>
2023-02-24 22:59:55 +0300

    add validation to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,6 @@
 class Reservation < ApplicationRecord
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :departure_city, presence: true, length: { in: 2..75 }
+
 end
