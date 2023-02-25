18b3cf0c39ee59acd0ef9d0a6d58e918596088fe
shakerAbuDrais <thecoffeiner@gmail.com>
2023-02-25 20:17:22 +0300

    Add end points so user can add new trip

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -10,6 +10,23 @@ module Api
         trip = Trip.find(params[:id])
         render json: trip
       end
+
+      def create
+        user = User.find(params[:user_id])
+        @trip = user.trips.build(trip_params)
+
+        if @trip.save
+          render json: @trip
+        else
+          render json: { error: 'Unable to create trip' }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def trip_params
+        params.require(:trip).permit(:price, :rating, :destination_city, :description)
+      end
     end
   end
 end
