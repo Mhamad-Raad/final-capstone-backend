36530918be73f221ad8918742c4283379b1c7c90
joker69216 <hamaraad883@gmail.com>
2023-02-26 18:45:12 +0300

    add reservation end point / merge with dev

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::ReservationsController < ApplicationController
+  def index 
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: { error: @reservation.errors.full_messages }, status: 400
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:name, :email, :phone, :date, :time, :guests)
+  end
 end
