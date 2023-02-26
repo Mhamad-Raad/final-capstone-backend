41d3f3e69a5763e6bef19ba6771be431f4e31f00
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-27 01:36:10 +0500

    Generate a token for authentication

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,21 @@
-module Api
-  class ApiController < ActionController::API
+class Api::ApiController < ActionController::API
+  include ExceptionHandler
+  before_action :authorize_request, except: [:login, :signup]
 
-  end  
+  def not_found
+    render json: { msg:'Unable to access token', error: 'Token not found' }, status: :unauthorized
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      not_found
+    end
+  end
 end
