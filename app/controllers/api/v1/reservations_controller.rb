ef03e891374c11161f24e5c6b0fac82f8c1c428a
joker69216 <hamaraad883@gmail.com>
2023-02-26 19:03:17 +0300

    update reservation parameters

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::ReservationsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def index 
     @reservations = Reservation.all
     render json: @reservations
   end
 
   def create
+    p reservation_params
+    p "hello there i am here"
     @reservation = Reservation.new(reservation_params)
     if @reservation.save
       render json: @reservation, status: :created
@@ -16,6 +21,6 @@ class Api::V1::ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:user_id, :date, :time, :departure_city)
+    params.require(:reservation).permit(:user_id, :date, :time, :departure_city, :trip_id)
   end
 end
