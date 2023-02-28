07ec553774aed0329fb216e92e1259f7a6bf8d4e
KanzaTahreem <tehreemmsk411@gmail.com>
2023-03-01 02:52:02 +0500

    Create an endpoint to delete an item

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -21,6 +21,17 @@ module Api
           render json: { error: 'Unable to create trip' }, status: :unprocessable_entity
         end
       end
+      
+      def destroy
+        @trip = Trip.find(params[:id])
+        @reservations = Reservation.where(trip_id: @trip.id)
+        @reservations.each(&:destroy)
+        if @trip.destroy
+          render json: {success: "Trip destroyed successfully"}, status: :ok
+        else
+          render :json, { error: 'Unable to destroy a trip' }, status: :unprocessable_entity
+        end       
+      end
 
       private
 
