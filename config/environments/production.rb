0a99a9591b3807173d26335b85166e5c01720049
shakerAbuDrais <thecoffeiner@gmail.com>
2023-03-06 13:48:09 +0300

    Edit prduction config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.secret_key_base = Rails.application.credentials.secret_key_base
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
