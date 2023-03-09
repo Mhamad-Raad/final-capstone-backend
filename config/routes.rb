19fa944ae8b20687f3796d3c97ff062ac02bf671
shakerAbuDrais <thecoffeiner@gmail.com>
2023-03-09 03:27:37 +0300

    Fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   default_url_options :host => "https://capstone-backend-gz9j.onrender.com"
+  if Rails.env.development?
+    mount SwaggerUiEngine::Engine, at: "/api-docs"
+  else
+    get "/api-docs", to: redirect("/api-docs/index.html?url=/api-docs/swagger.yaml")
+  end
   namespace :api do
     namespace :v1 do
       post 'auth/sign_up', to: 'authentication#signup'
